02bb86fd0b7e743b81c791709a450a004681e58c
Zpeugh <zach.peugh@spidasoftware.com>
2016-04-07 22:35:58 -0400

    Adding failure probability displayed to screen

--- Lab2/log_reg.m
+++ Lab2/log_reg.m
@@ -1,3 +1,4 @@
+clc
 %Read in the data
 data = importdata('logistic.txt');
 
@@ -16,12 +17,16 @@ xx = linspace(25, max(X)+ 10);
 %get predicted probabilities for the xx values
 ypred = glmval(b,xx,'logit');
 
+%get the probability of failure for a temperature of 28 degrees
+fail_prob = glmval(b,28, 'logit');
+fprintf('Predicted probability of failure at 28 degrees: %.2f%%\n', fail_prob * 100);
+
 %plot the original binary response variables and the predicted regression
 hold on;
 figure(1);
 plot(X,Y,'blacko',...
     xx,ypred,'b-', 'MarkerFace', 'black');
-plot(28, glmval(b,28, 'logit'), 'r*', 'MarkerSize', 12);
+plot(28, fail_prob, 'r*', 'MarkerSize', 12);
 axis([25 90 -.15 1.15]);
 ylabel('Probability of O-ring failure');
 xlabel('Temperature (F)');
